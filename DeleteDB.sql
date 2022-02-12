e8cf388974feb71a91be3ff7e42b45dce98743ec
Polumm <songchujiashenzhen@163.com>
2022-02-12 08:34:17 +0800

    2.2.5

---
+++ DeleteDB.sql
@@ -0,0 +1,15 @@
+drop table 绩点模型
+drop table 学生密码表
+drop table 管理员密码表
+drop table 教师密码表
+drop table 课程学生SC
+drop table 排课表
+drop table 课程
+drop table 教室
+drop table 学生
+drop table 班
+drop table 教师
+drop table 系
+
+
+
